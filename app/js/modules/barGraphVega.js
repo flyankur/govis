a46a37ddc2811b30037b8db75a5f0603b2fe7bbd
ankur <drecodeam@gmail.com>
2013-10-14 16:02:53 +0530

    Added data module

--- app/js/modules/barGraphVega.js
+++ app/js/modules/barGraphVega.js
@@ -1,44 +1,46 @@
 /**
  * Module that returns the vega for a bar graph
  * ### TODO : Need to give it parameters for various properties
+ *            see this https://github.com/trifacta/vega/wiki
+ *
  * @return {Vega Spec}    Returns the specification for a bar graph vega
  */
-define( function( vg ) {
+define( function() {
     var spec = {
-        'width'  : 400,
-        'height' : 200,
-        'padding': {'top': 10, 'left': 30, 'bottom': 30, 'right': 10},
-        'data'   : [{'name': 'table'}],
-        'scales' : [
+        "width"  : 400,
+        "height" : 200,
+        "padding": {"top": 10, "left": 30, "bottom": 30, "right": 10},
+        "data"   : [{"name": "table"}],
+        "scales" : [
             {
-                'name': 'x', 'type': 'ordinal', 'range': 'width',
-                'domain': {'data': 'table', 'field': 'data.x'}
+                "name": "x", "type": "ordinal", "range": "width",
+                "domain": {"data": "table", "field": "data.x"}
             },
             {
-                'name': 'y', 'range': 'height', 'nice': true,
-                'domain': {'data': 'table', 'field': 'data.y'}
+                "name": "y", "range": "height", "nice": true,
+                "domain": {"data": "table", "field": "data.y"}
             }
         ],
-        'axes': [
-            {'type': 'x', 'scale': 'x'},
-            {'type': 'y', 'scale': 'y'}
+        "axes": [
+            {"type": "x", "scale": "x"},
+            {"type": "y", "scale": "y"}
         ],
-        'marks': [
+        "marks": [
             {
-                'type': 'rect',
-                'from': {'data': 'table'},
-                'properties': {
-                    'enter': {
-                        'x': {'scale': 'x', 'field': 'data.x'},
-                        'y': {'scale': 'y', 'field': 'data.y'},
-                        'y2': {'scale': 'y', 'value': 0},
-                        'width': {'scale': 'x', 'band': true, 'offset': -1}
+                "type": "rect",
+                "from": {"data": "table"},
+                "properties": {
+                    "enter": {
+                        "x": {"scale": "x", "field": "data.x"},
+                        "y": {"scale": "y", "field": "data.y"},
+                        "y2": {"scale": "y", "value": 0},
+                        "width": {"scale": "x", "band": true, "offset": -1}
                     },
-                    'update': {
-                        'fill': {'value': 'steelblue'}
+                    "update": {
+                        "fill": {"value": "steelblue"}
                     },
-                    'hover': {
-                        'fill': {'value': 'red'}
+                    "hover": {
+                        "fill": {"value": "red"}
                     }
                 }
             },
